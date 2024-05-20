06beb10a39a515f5d3ba13f9d18b40436f7fab43
mirkoWolf <mirkowolf46@gmail.com>
2024-05-20 01:17:56 -0300

    Update Guia6.py

---
+++ Python/Guia6.py
@@ -0,0 +1,29 @@
+import math
+
+# Ej 1
+
+
+def imprimir_hola_mundo():
+    print("Hola mundo")
+
+
+def imprimir_un_verso():
+    print("Árbol, hoja, salto, luz \nAproximación \nMueble, lana, gusto, pie \nTé, mar, gas, mirada")
+
+
+def raizDe2():
+    print(round(math.sqrt(2), 4))
+
+
+def factorial_de_dos():
+    print(math.factorial(2))
+
+
+def perimetro():
+    print(round(2 * math.pi, 4))
+
+# Ej 2
+
+
+def imprimir_saludo(nombre: str):
+    print("Hola " + nombre)
